1a08b1531830bde61586b8e89f160eb2aca31b68
Dipsan Dhital <dipsandhital12@gmail.com>
2026-08-20 19:36:40 +0545

    Sign In Ui

--- app/src/main/java/com/example/healthpal/SplashScreen.java
+++ app/src/main/java/com/example/healthpal/SplashScreen.java
@@ -1,6 +1,9 @@
 package com.example.healthpal;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +12,23 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class SplashScreen extends AppCompatActivity {
+    Button continue_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_splash_screen);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        continue_btn = findViewById(R.id.btnContinue);
+
+        continue_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SplashScreen.this,CreateAccount.class);
+                startActivity(intent);
+            }
         });
+
     }
 }
