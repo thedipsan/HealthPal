7f19175e6340aa84dac844510bd0833a525386f8
Dipsan Dhital <dipsandhital12@gmail.com>
2026-08-21 16:14:45 +0545

    Forgot Password Backend Implementation

--- app/src/main/java/com/example/healthpal/ForgotPassword.java
+++ app/src/main/java/com/example/healthpal/ForgotPassword.java
@@ -1,24 +1,58 @@
 package com.example.healthpal;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class ForgotPassword extends AppCompatActivity {
 
+    EditText etEmail;
+    Button btnForgetPassword;
+
+    FirebaseAuth firebaseAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_forgot_password);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        firebaseAuth = FirebaseAuth.getInstance();
+         etEmail = findViewById(R.id.etForgotEmail);
+         btnForgetPassword = findViewById(R.id.btnForgotPassword);
+
+
+         btnForgetPassword.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 String email = etEmail.getText().toString().trim();
+
+                 if(email.isEmpty()){
+                     Toast.makeText(ForgotPassword.this, "Please Enter your Email Address", Toast.LENGTH_SHORT).show();
+                 }
+                 firebaseAuth.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
+                     @Override
+                     public void onComplete(@NonNull Task<Void> task) {
+                         if(task.isSuccessful()){
+                             Toast.makeText(ForgotPassword.this, "Please Check your mail", Toast.LENGTH_SHORT).show();
+                             finish();
+                         }
+
+                     }
+                 });
+             }
+         });
     }
 }
